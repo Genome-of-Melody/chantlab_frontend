1d823c7b9300000878f06dbd00e77119732c6f1c
lanzv <vojtech.lanz@gmail.com>
2024-12-19 14:28:27 +0100

    deleting default datasets cantus corpus and netvor not allowed

---
+++ src/app/components/dialogs/default-datasets-not-deletable-dialog/default-datasets-not-deletable-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-default-dataset-not-deletable-dialog',
+  templateUrl: './default-datasets-not-deletable-dialog.component.html',
+  styleUrls: ['./default-datasets-not-deletable-dialog.component.css']
+})
+export class DefaultDatasetsNotDeletableDialogComponent {
+
+}
